a88e642258c473c311d62a6ab7724489ff123bd7
Edgar Fenollar <147927974+EdgarFenollar@users.noreply.github.com>
2025-03-26 10:04:16 +0100

    Initial commit

---
+++ src/main/java/com/project/hstime/HstimeApplication.java
@@ -0,0 +1,13 @@
+package com.project.hstime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HstimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HstimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/hstime/HstimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hstime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HstimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
